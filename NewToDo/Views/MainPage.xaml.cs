7ed9779115c65fce6ab0e59f43c6d21b7cee826d
MRX-ar <85848152+MRX-ar@users.noreply.github.com>
2022-03-10 00:36:02 +0300

    Добавьте файлы проекта.

---
+++ NewToDo/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NewToDo.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void MyDay(object sender, System.EventArgs e )
+        {
+
+        }
+    }
+}
